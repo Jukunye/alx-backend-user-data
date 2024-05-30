f43bf68b36c34b629b39d9a6adb6c2fca791895e
Jukunye <jukunyes@gmail.com>
2024-05-30 22:51:15 +0300

    refactor: find_user_by method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,8 +4,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError, NoResultFound
-from typing import Dict, Any
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+# from typing import Dict, Any
 
 from user import Base
 from user import User
