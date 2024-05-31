b0282041d581829575c86be21f6fc2f4f79301d3
Jukunye <jukunyes@gmail.com>
2024-05-31 03:30:54 +0300

    Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 This module contains the flask app
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 app = Flask(__name__)
@@ -27,6 +27,21 @@ def register():
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"})
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """
+    Session login flask
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if (AUTH.valid_login(email, password)):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
+    abort(401)
 
 
 if __name__ == '__main__':
