944a50b4b8e68d336545771927de3be20be81959
Jukunye <jukunyes@gmail.com>
2024-05-31 04:25:56 +0300

    User profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -62,5 +62,22 @@ def logout() -> str:
     return redirect('/')
 
 
+@app.route('/profile', methods=['GET'])
+def profile() -> str:
+    """
+    Get user profile information based on session ID
+    """
+
+    session_id = request.cookies.get('session_id')
+    if session_id is None:
+        abort(403)
+
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email})
+    else:
+        abort(403)
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
