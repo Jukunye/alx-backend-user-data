2f9012161e38e5e4c0bae761358f15c58591ca26
Jukunye <jukunyes@gmail.com>
2024-05-31 05:12:55 +0300

    refactor: main.py

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -144,7 +144,9 @@ def update_password(email: str, reset_token: str, new_password: str) -> None:
     """
     url = f'{BASE_URL}/reset_password'
     response = requests.put(url, data={
-                            'email': email, 'reset_token': reset_token, 'new_password': new_password})
+                            'email': email,
+                            'reset_token': reset_token,
+                            'new_password': new_password})
     assert response.status_code == 200
     assert response.json()['message'] == 'Password updated'
 
