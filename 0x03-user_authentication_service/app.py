5641b67af46628e5b0309f3c64813cd1ce63d405
Jukunye <jukunyes@gmail.com>
2024-05-31 03:55:16 +0300

    Log out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 This module contains the flask app
 """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 app = Flask(__name__)
@@ -42,6 +42,24 @@ def login():
         response.set_cookie("session_id", session_id)
         return response
     abort(401)
+
+
+@app.route('/sessions', methods=['DELETE'])
+def logout() -> str:
+    """
+    Endpoint that destroys a session
+    """
+
+    session_id = request.cookies.get("session_id", None)
+    if session_id is None:
+        abort(403)
+
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+
+    AUTH.destroy_session(user.id)
+    return redirect('/')
 
 
 if __name__ == '__main__':
